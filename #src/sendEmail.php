03326e798236a22bdaf27f2fd6dc3cf3965cc9c4
Roman <roman.sav15@gmail.com>
2021-02-17 15:10:55 +0200

    new pages, fixes, contact form

---
+++ #src/sendEmail.php
@@ -0,0 +1,23 @@
+<?php
+$to      = 'roman.orangeorange@gmail.com';
+$subject = 'GoldenWebAge';
+$useremail = $_POST['callback_email'];
+$username = $_POST['callback_name'];
+$message = $_POST['callback_message'];
+$c = "\r\n";
+
+$text_message = 'Name: ' . $username . $c .
+'Email: ' . $useremail . $c .
+'Message: ' . $message;
+
+$headers = 'From: goldenwebage.ch' . "\r\n" .
+    'X-Mailer: PHP/' . phpversion();
+mail($to, $subject, $text_message, $headers);
+
+if (mail($to, $subject, $text_message, $headers)) {
+      header('location: thankyou.html');
+    } else {
+      echo 'Error';
+    }
+
+?>
