8eb8a02ef4f6dbb1e4be17fab7a6fa3bea9cbb07
Roman <roman.sav15@gmail.com>
2021-02-23 11:16:09 +0200

    fixed responsive issues, added contact form

--- #src/sendEmail.php
+++ #src/sendEmail.php
@@ -1,23 +1,45 @@
 <?php
-$to      = 'info@goldenwebage.de';
-$subject = 'GoldenWebAge';
-$useremail = $_POST['callback_email'];
-$username = $_POST['callback_name'];
-$message = $_POST['callback_message'];
-$c = "\r\n";
+    use PHPMailer\PHPMailer\PHPMailer;
 
-$text_message = 'Name: ' . $username . $c .
-'Email: ' . $useremail . $c .
-'Message: ' . $message;
 
-$headers = 'From: goldenwebage.ch' . "\r\n" .
-    'X-Mailer: PHP/' . phpversion();
-mail($to, $subject, $text_message, $headers);
+    $name = $_POST['callback_name'];
+    $email = $_POST['callback_email'];
+    $message = $_POST['callback_message'];
 
-if (mail($to, $subject, $text_message, $headers)) {
+    // $attachment = $_POST['upload'];
+
+    require_once "PHPMailer/PHPMailer.php";
+    require_once "PHPMailer/SMTP.php";
+    require_once "PHPMailer/Exception.php";
+
+    $mail = new PHPMailer();
+
+    //SMTP Settings
+    $mail->isSMTP();
+    $mail->Host = "smtp.gmail.com";
+    $mail->SMTPAuth = true;
+    $mail->Username = ""; // sender mail (SMPT)
+    $mail->Password = ''; // password of sender mail
+    $mail->Port = 465;
+    $mail->SMTPSecure = "ssl";
+
+    //Email Settings
+    $mail->addAttachment($_FILES['upload']['tmp_name'], $_FILES['upload']['name']);
+    $mail->isHTML(true);
+
+    $mail->setFrom(''); // sender mail 
+    $mail->addAddress("info@goldenwebage.de"); // where the emails will go
+    $mail->Subject = ("$email ($name)");
+    $mail->Body = 'Name: ' . $name . '<br>' .
+    'Email: ' . $email . '<br> <br>' .
+    'Message: ' . $message . '<br>';
+
+
+    if ($mail->send()) {
       header('location: thankyou.html');
     } else {
       echo 'Error';
     }
 
+    exit(json_encode(array("status" => $status, "response" => $response)));
 ?>
