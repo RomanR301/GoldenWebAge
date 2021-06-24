5f3a8f1005b509874716d84189a6d605258ab03c
Roman <roman.sav15@gmail.com>
2021-06-24 09:54:14 +0300

    blog categories

--- #src/js/script.js
+++ #src/js/script.js
@@ -86,20 +86,7 @@ let front = {
       document.getElementById(tabName).style.display = "block";
       $(element).addClass('active');
   },
-  hoverTab: function (el, tabName) {
-    var i, tabcontent, tablinks;
-    tabcontent = document.getElementsByClassName("tabcontent");
-    for (i = 0; i < tabcontent.length; i++) {
-        tabcontent[i].style.display = "none";
-    }
-    tablinks = document.getElementsByClassName("tablinks");
-    for (i = 0; i < tablinks.length; i++) {
-      tablinks[i].className = tablinks[i].className.replace(" active", "");
-    }
 
-    document.getElementById(tabName).style.display = "block";
-    el.currentTarget.className += " active";
-  },
 
   events: function () {
       let self = this;
